df4506785d6a794e8acbebc6e742193d86a79e5c
Novita Pratiwi <novitapratiwi8@gmail.com>
2021-06-01 15:40:00 +0700

    menghubungkan layout kesetiap page

---
+++ resources/views/pages/laporan/index.blade.php
@@ -0,0 +1,51 @@
+@extends('layouts.app')
+
+@section('custom_styles')
+
+@endsection
+
+@section('prepend_script')
+
+@endsection
+
+@section('append_script')
+
+@endsection
+
+@section('title')
+    Laporan
+@endsection
+
+@section('content')
+<div class="content-header row">
+    <div class="content-header-left col-md-9 col-12 mb-2">
+        <div class="row breadcrumbs-top">
+            <div class="col-12">
+                <h2 class="content-header-title float-left mb-0">Home</h2>
+
+            </div>
+        </div>
+    </div>
+    <div class="content-header-right text-md-right col-md-3 col-12 d-md-block d-none">
+        <div class="form-group breadcrum-right">
+
+        </div>
+    </div>
+</div>
+<div class="content-body">
+    <!-- Zero configuration table -->
+    <section id="basic-datatable">
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-header pb-3">
+                        <h4 class="card-title">Selamat Datang User</h4>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--/ Zero configuration table -->
+</div>
+@endsection
