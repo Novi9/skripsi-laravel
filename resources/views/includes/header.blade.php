61283c09921e519044a96470212f5cd781478d64
Novita Pratiwi <novitapratiwi8@gmail.com>
2021-06-15 22:35:39 +0700

    membuat landing page

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -4,6 +4,19 @@
             <div class="navbar-collapse" id="navbar-mobile">
                 <div class="mr-auto float-left bookmark-wrapper d-flex align-items-center">
 
+                    <div class="card-content">
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-15">
+                                <nav class="menu main menu container">
+                                    <ul id="menu-main-menu-1" class="menu">
+                                        <button type="button" class="btn btn-flat-primary ml-2 mb-1">Home</button>
+                                    <button type="button" class="btn btn-flat-primary ml-2 mb-1">AHP-TOPSIS</button>
+                                    <button type="button" class="btn btn-flat-primary ml-2 mb-1">Tentang</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <ul class="nav navbar-nav float-right">
                     <li class="dropdown dropdown-user nav-item"><a class="dropdown-toggle nav-link dropdown-user-link" href="#" data-toggle="dropdown">
--- resources/views/pages/home/index.blade.php
+++ resources/views/pages/home/index.blade.php
@@ -21,7 +21,8 @@
     <div class="content-header-left col-md-9 col-12 mb-2">
         <div class="row breadcrumbs-top">
             <div class="col-12">
-                <h2 class="content-header-title float-left mb-0">Home</h2>
+                <h2 class="content-header-title float-left mb-0 mt-2">SELAMAT DATANG</h2>
+                <h1 class="content-header-title float-left mb-0 mt-2">Penerapan Metode AHP dan TOPSIS Pada Seleksi Penerimaan Karyawan Baru Pada Ma'had Abu Ubaidah Bin Al-Jarrah Medan</h1>
             </div>
         </div>
     </div>
@@ -35,46 +36,8 @@
     <div class="row">
         <div class="col-12">
             <div class="card">
-                <div class="card-header">
-                    <h4 class="card-title">SELAMAT DATANG ADMIN</h4>
-                </div>
-                <div class="card-content">
-                    <div class="card-body">
-                        <div class="row">
-                            <div class="col-12">
-                                <button type="button" class="btn btn-flat-primary mr-1 mb-1">Home</button>
-                                <button type="button" class="btn btn-flat-success mr-1 mb-1">Success</button>
-                                <button type="button" class="btn btn-flat-info mr-1 mb-1">Info</button>
-                                <button type="button" class="btn btn-flat-warning mr-1 mb-1">Warning</button>
-                                <button type="button" class="btn btn-flat-danger mr-1 mb-1">Danger</button>
-                                <button type="button" class="btn btn-flat-light mr-1 mb-1">Light</button>
-                                <button type="button" class="btn btn-flat-dark mr-1 mb-1">Dark</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
             </div>
         </div>
     </div>
 </section>
-
-<div class="content-body">
-    <!-- Zero configuration table -->
-    <section id="basic-datatable">
-        <div class="row">
-            <div class="col-12">
-                <div class="card">
-                    <div class="card-header pb-3">
-                        <h4 class="card-title justify-content-center">SELAMAT DATANG ADMIN</h4>
-                        <p class="mb-0">Penerapan Metode AHP dan TOPSIS Pada Seleksi Penerimaan Karyawan Baru Pada Ma'had Abu Ubaidah Bin Al-Jarrah Medan</p>
-                    </div>
-
-                </div>
-            </div>
-        </div>
-    </section>
-
-
-
-</div>
 @endsection
